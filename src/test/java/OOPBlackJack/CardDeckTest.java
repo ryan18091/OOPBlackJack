06d644fbf0ce0fed40697907d3e60d534045c16e
Ryan Erickson <ryanerickson74@gmail.com>
2018-03-06 13:35:23 -0600

    Updated Tests

---
+++ src/test/java/OOPBlackJack/CardDeckTest.java
@@ -0,0 +1,16 @@
+package OOPBlackJack;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CardDeckTest {
+    @Test
+    public void getNextCard() throws Exception {
+    }
+
+    @Test
+    public void shuffle() throws Exception {
+    }
+
+}
